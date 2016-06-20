e679e4a9b012cdfa112279ca4712e4e5ca5f0e44
techme <techme@live.cn>
2016-06-21 00:01:36 +0800

    经过和科技宽带不懈的斗争，我成功的生成了博客，免费host对我来说是无比巨大的诱惑呀

---
+++ js/tipuesearch/tipuesearch_content.js
@@ -0,0 +1,85 @@
+var tipuesearch = {"pages": [
+    {"title": "Home",
+    "text": "Home.",
+    "tags": "home, index",
+    "url": "http://codeinpink.github.io/"},
+    {"title": "About Me",
+    "text": "A little about me, including my interests and some random facts!",
+    "tags": "about, interests, random facts",
+    "url": "http://codeinpink.github.io/#/about"},
+    {"title": "Resume",
+    "text": "An overview of my resume, including my education, work history, development skills, and more.",
+    "tags": "resume, skills, education, school, work, experience, awards, honors, languages, frameworks, databases",
+    "url": "http://codeinpink.github.io/#/resume"},
+    {"title": "Blog",
+    "text": "My latest thoughts, posts, and inspirations.",
+    "tags": "blog, latest posts, tutorials",
+    "url": "http://codeinpink.github.io/blog"},
+    {"title": "Portfolio",
+    "text": "My portfolio of projects that I can show.",
+    "tags": "portfolio, projects",
+    "url": "http://codeinpink.github.io/#/portfolio"},
+    {"title": "Contact",
+    "text": "Feel free to email me or fill out the contact form to get in touch.",
+    "tags": "contact, form, email",
+    "url": "http://codeinpink.github.io/#/contact"},
+
+    
+    
+    {"title": "Babincsak Martial Arts",
+    "text": "A website for my family’s business, Babincsak Martial Arts. I designed and built this site, and I was responsible for 90% of the site’s content. The site, along with the facebook page, is now a major part in new student sign-ups. My father founded Babincsak Martial Arts in the fall… ",
+    "tags": "web development, project, HTML, CSS, JavaScript, PHP, Bootstrap, SEO, Photo Optimization, Responsive, and Mobile Friendly",
+    "url": "http://codeinpink.github.io/portfolio/babincsak-martial-arts"},
+    
+    
+    {"title": "codeinpink.github.io",
+    "text": "My website containing info about me, my projects, and my blog. I’ve wanted a site like this for a long time, but it’s been put off due to school, work, other projects, and laziness. After I finished my last project, I forced myself to work on and finish this so… ",
+    "tags": "web development, project, Jekyll, Ruby, JavaScript, HTML, CSS, jQuery, AJAX, Git, Responsive, and Mobile Friendly",
+    "url": "http://codeinpink.github.io/portfolio/codeinpinkgithubio"},
+    
+    
+    {"title": "MovieLib",
+    "text": "A website used to track your movie collection. I chose this project because I wanted to learn a back end MVC framework, and I originally picked Ruby on Rails, but learning both a framework and a new language wasn’t so easy, so instead I opted for Django, the Python, MVT… ",
+    "tags": "web development, project, Python, Django, JavaScript, HTML, CSS, Bootstrap, jQuery, PostgreSQL, AJAX, Git, Responsive, and Mobile Friendly",
+    "url": "http://codeinpink.github.io/portfolio/movielib"},
+    
+    
+    {"title": "PSI Planning Tool",
+    "text": "A web application for PSI planning. Import a list of teams and a list of features, and each team can then add commitments to the features they’ve been assigned, features belonging to other teams, or new features they add themselves. Teams can then add risks and dependencies for each commitment,… ",
+    "tags": "web development, project, Python, Django, JavaScript, Angular, HTML, CSS, Bootstrap, Git, Docker, Responsive, and Mobile Friendly",
+    "url": "http://codeinpink.github.io/portfolio/psi-planning-tool"},
+    
+    
+    {"title": "Putnam County Sheriff's Office",
+    "text": "A website that I designed and built for Putnam County’s Sheriff’s Office as an intern. Since the site would need to be updated frequently for pictures, news and events, etc., Wordpress seemed like the ideal solution. I learned Wordpress and used HTML, CSS, PHP, and Bootstrap to make the site…. ",
+    "tags": "web development, project, HTML, CSS, JavaScript, PHP, Wordpress, Bootstrap, Responsive, and Mobile Friendly",
+    "url": "http://codeinpink.github.io/portfolio/putnam-county-sheriffs-office"},
+    
+
+    
+    {"title": "My Latest Z-index Problem",
+    "text": "Two months ago I launched a site for a gaming project that I&#8217;m involved in.  I received a lot of good feedback from people, but it wasn&#8217;t long before I had my first bug report, something I was expecting from my lack of browser testing: the navigation dropdown on iPhones was behind the home page content.  Great.  ",
+    "tags": "web development, css and ios",
+    "url": "http://codeinpink.github.io/blog/2016/05/my-latest-z-index-problem"},
+    
+    {"title": "My Second Term at ADTRAN: First Month",
+    "text": "I returned in August for my second term as a co-op, and this term I got the UX/UI team, a team I really wanted. Over the summer, I learned Django, a MVT back end framework, but I didn&#8217;t know any MVC front end frameworks yet. Spending a term with these guys would definitely teach me more about front end development since that&#8217;s all I would be doing! Well, that&#8217;s what I thought, at least. The first month has actually been an interesting mix of work. I&#8217;m supposed to develop the front end for key products using Angular, but outside of ensuring cross-browser compatibility, all of my development has been for miscellaneous (and unplanned) things!  ",
+    "tags": "general, work, wordpress, angular, django, and hackathon",
+    "url": "http://codeinpink.github.io/blog/2015/09/my-second-term-at-adtran-first-month"},
+    
+    {"title": "My First Term at ADTRAN",
+    "text": "When I accepted an offer to co-op at ADTRAN, I was initially worried about what I&#8217;d work on. ADTRAN is in the telecommunications industry, so most of their software developers probably work on embedded programming. My resume had a lot on it, but all of the recent things were web-related, and I wanted to learn back end web development. However, I also wanted to work on something that wouldn&#8217;t just be a typical CRUD application. Would I find a fit? Would I enjoy my work?  ",
+    "tags": "general, work",
+    "url": "http://codeinpink.github.io/blog/2015/07/my-first-term-at-adtran"},
+    
+    {"title": "Adding Autocomplete/Suggestions to Inputs Through Twitter's Typeahead",
+    "text": "I recently used Twitter&#8217;s JavaScript library typeahead.js (now referred to as typeahead in the rest of the post) in one of my projects, MovieLib, and I can&#8217;t wait to use it again! Once I understood how to use it, it was easy to implement it into my project, and I think it really improved the user experience in a few areas of the site.  ",
+    "tags": "web development, javascript, tutorial, typeahead, and jQuery",
+    "url": "http://codeinpink.github.io/blog/2015/07/adding-autocomplete-to-inputs-through-twitters-typeahead"},
+    
+    {"title": "Obligatory &quot;Hello World&quot;",
+    "text": "Just like how every programmer first starts with a &#8220;Hello World&#8221; program, it&#8217;s basically mandatory for &#8220;Hello World&#8221; to be every programmer&#8217;s first blog post!  So, here it is: Hello World!  ",
+    "tags": "general, ",
+    "url": "http://codeinpink.github.io/blog/2015/07/obligatory-hello-world"}
+    
+]};
