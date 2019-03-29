89869b59e269c1a283305c304ca5b425afd88b26
于志强 <1963260657@qq.com>
2019-03-29 14:37:22 +0800

    更新欢迎语
    Signed-off-by: 于志强 <1963260657@qq.com>

--- live2d/js/message.js
+++ live2d/js/message.js
@@ -23,12 +23,12 @@ String.prototype.renderTip = function (context) {
 var re = /x/;
 console.log(re);
 re.toString = function() {
-    showMessage('哈哈，你打开了控制台，是想要看看我的秘密吗？', 5000);
+    showMessage('里面～空荡荡的', 5000);
     return '';
 };
 
 $(document).on('copy', function (){
-    showMessage('你都复制了些什么呀，转载要记得加上出处哦~~', 5000);
+    showMessage('拿走拿走别客气……', 5000);
 });
 
 function initTips(){
@@ -76,26 +76,26 @@ initTips();
         if (window.location.href == `${home_Path}`) { //主页URL判断，需要斜杠结尾
             var now = (new Date()).getHours();
             if (now > 23 || now <= 5) {
-                text = '你是夜猫子呀？这么晚还不睡觉，明天起的来嘛？';
+                text = '睡你麻痹？起来嗨！';
             } else if (now > 5 && now <= 7) {
-                text = '早上好！一日之计在于晨，美好的一天就要开始了！';
+                text = '早上好！美好的一天就要开始搬砖了！';
             } else if (now > 7 && now <= 11) {
-                text = '上午好！工作顺利嘛，不要久坐，多起来走动走动哦！';
+                text = '上午好！打卡没？工头喊你干活呢！';
             } else if (now > 11 && now <= 14) {
-                text = '中午了，工作了一个上午，现在是午餐时间！';
+                text = '中午了，中午不睡下午崩溃！';
             } else if (now > 14 && now <= 17) {
-                text = '午后很容易犯困呢，今天的运动目标完成了吗？';
+                text = '怼完产品，怼测试，然后再怼运维';
             } else if (now > 17 && now <= 19) {
-                text = '傍晚了！窗外夕阳的景色很美丽呢，最美不过夕阳红~~';
+                text = '傍晚了！上班没搞定的Bug，加班也搞不定';
             } else if (now > 19 && now <= 21) {
-                text = '晚上好，今天过得怎么样？';
+                text = '晚上好，这时候写的代码BUG翻倍～';
             } else if (now > 21 && now <= 23) {
-                text = '已经这么晚了呀，早点休息吧，晚安~~';
+                text = '已经这么晚了呀，回家已经没地铁了~~';
             } else {
-                text = '嗨~ 快来逗我玩吧！';
+                text = '嗨~ 你瞅啥，再瞅削你！';
             }
         }else {
-            text = '欢迎阅读<span style="color:#0099cc;">「 ' + document.title.split(' - ')[0] + ' 」</span>';
+            text = '看了也白看，真的～<span style="color:#0099cc;">「 ' + document.title.split(' - ')[0] + ' 」</span>';
         }
     }
     showMessage(text, 12000);
