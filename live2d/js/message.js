9069ff8e6764ff0c8a2d76ae18ce4be46467f8e2
于志强 <1963260657@qq.com>
2019-04-29 11:27:38 +0800

    修改部分逻辑
    Signed-off-by: 于志强 <1963260657@qq.com>

--- live2d/js/message.js
+++ live2d/js/message.js
@@ -19,9 +19,12 @@ function renderTip(template, context) {
 String.prototype.renderTip = function (context) {
     return renderTip(this, context);
 };
-
+var synth = window.speechSynthesis;
+var u = new SpeechSynthesisUtterance();
+u.lang = 'zh-CN';
+u.rate = 1;
+u.volume = 1;
 var re = /x/;
-console.log(re);
 re.toString = function () {
     showMessage('里面～空荡荡的', 5000);
     return '';
@@ -75,29 +78,25 @@ initTips();
             text = '嗨！ 来自 谷歌翻墙 的朋友！<br>欢迎访问<span style="color:#0099cc;">「 ' + document.title.split(' - ')[0] + ' 」</span>';
         }
     } else {
-        if (window.location.href == `${home_Path}`) { //主页URL判断，需要斜杠结尾
-            var now = (new Date()).getHours();
-            if (now > 23 || now <= 5) {
-                text = '睡你麻痹？起来嗨！';
-            } else if (now > 5 && now <= 7) {
-                text = '早上好！美好的一天就要开始搬砖了！';
-            } else if (now > 7 && now <= 11) {
-                text = '上午好！打卡没？工头喊你干活呢！';
-            } else if (now > 11 && now <= 14) {
-                text = '中午了，中午不睡下午崩溃！';
-            } else if (now > 14 && now <= 17) {
-                text = '怼完产品，怼测试，然后再怼运维';
-            } else if (now > 17 && now <= 19) {
-                text = '傍晚了！上班没搞定的Bug，加班也搞不定';
-            } else if (now > 19 && now <= 21) {
-                text = '晚上好，这时候写的代码BUG翻倍～';
-            } else if (now > 21 && now <= 23) {
-                text = '已经这么晚了呀，回家已经没地铁了~~';
-            } else {
-                text = '嗨~ 你瞅啥，再瞅削你！';
-            }
+        var now = (new Date()).getHours();
+        if (now > 23 || now <= 5) {
+            text = '睡你麻痹？起来嗨！';
+        } else if (now > 5 && now <= 7) {
+            text = '早上好！美好的一天就要开始搬砖了！';
+        } else if (now > 7 && now <= 11) {
+            text = '上午好！打卡没？工头喊你干活呢！';
+        } else if (now > 11 && now <= 14) {
+            text = '中午了，中午不睡下午崩溃！';
+        } else if (now > 14 && now <= 17) {
+            text = '怼完产品，怼测试，然后再怼运维';
+        } else if (now > 17 && now <= 19) {
+            text = '傍晚了！上班没搞定的Bug，加班也搞不定';
+        } else if (now > 19 && now <= 21) {
+            text = '晚上好，这时候写的代码BUG翻倍～';
+        } else if (now > 21 && now <= 23) {
+            text = '已经这么晚了呀，回家已经没地铁了~~';
         } else {
-            text = '看了也白看，真的～<span style="color:#0099cc;">「 ' + document.title.split(' - ')[0] + ' 」</span>';
+            text = '嗨~ 你瞅啥，再瞅削你！';
         }
     }
     showMessage(text, 12000);
@@ -111,18 +110,13 @@ function showHitokoto() {
         readMessage(result.hitokoto);
     });
 }
-var synth = window.speechSynthesis;
-var u = new SpeechSynthesisUtterance();
-u.lang = 'zh-CN';
-u.rate = 1;
-u.volume = 1;
-function readMessage(text){
+
+function readMessage(text) {
     u.text = text;
     synth.speak(u)
 }
 function showMessage(text, timeout) {
     if (Array.isArray(text)) text = text[Math.floor(Math.random() * text.length + 1) - 1];
-    //console.log('showMessage', text);
     $('.message').stop();
     $('.message').html(text).fadeTo(200, 1);
     if (timeout === null) timeout = 5000;
